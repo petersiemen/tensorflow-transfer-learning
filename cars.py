1579a9b3c60ce908f68b26cbc150ffc451879a1e
petersiemen <peter.siemen@googlemail.com>
2019-12-18 18:30:37 +0100

    dataset creation wip

---
+++ cars.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+keras = tf.keras
+import tensorflow_datasets as tfds
---
+++ convert_to_tf_records.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+import argparse
+import json
+import sys
+import os
+
+from object_detection.utils import dataset_util
+
+flags = tf.app.flags
+flags.DEFINE_string('input_json', '', 'Path to feed.json')
+flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_integer('limit', None, 'Number of dataset instances to create')
+FLAGS = flags.FLAGS
+
+
+def parse_function(filename, height, width):
+    image_string = tf.read_file(filename)
+
+    #Don't use tf.image.decode_image, or the output shape will be undefined
+    image = tf.image.decode_jpeg(image_string, channels=3)
+    #This will convert to float values in [0, 1]
+    image = tf.image.convert_image_dtype(image, tf.float32)
+
+    resized_image = tf.image.resize_images(image, [height, width])
+    return resized_image
+
+
+def create_tf_example(dataset_dir, obj):
+    image_name = obj['image']
+    full_image_path = os.path.join(dataset_dir, image_name)
+
+    # TODO(user): Populate the following variables from your example.
+    height = None  # Image height
+    width = None  # Image width
+    filename = None  # Filename of the image. Empty if image is not from file
+    encoded_image_data = None  # Encoded image bytes
+    image_format = None  # b'jpeg' or b'png'
+
+    xmins = []  # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = []  # List of normalized right x coordinates in bounding box
+    # (1 per box)
+    ymins = []  # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = []  # List of normalized bottom y coordinates in bounding box
+    # (1 per box)
+    classes_text = []  # List of string class name of bounding box (1 per box)
+    classes = []  # List of integer class id of bounding box (1 per box)
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def main(_):
+
+    writer = tf.io.TFRecordWriter(FLAGS.output_path)
+    limit = FLAGS.limit
+    # TODO(user): Write code to read in your dataset to examples variable
+
+    json_file = FLAGS.input_json
+    dataset_dir = os.path.dirname(json_file)
+    with open(json_file) as f:
+        for i, line in enumerate(f):
+            obj = json.loads(line)
+            tf_example = create_tf_example(dataset_dir, obj)
+            writer.write(tf_example.SerializeToString())
+
+            if limit is not None and i > limit:
+                break
+
+    writer.close()
+    #
+    # for example in examples:
+    #     tf_example = create_tf_example(example)
+    #     writer.write(tf_example.SerializeToString())
+    #
+    # writer.close()
+
+
+if __name__ == '__main__':
+    tf.app.run()
